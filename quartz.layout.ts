a1cd411895f59b762260670904426ee993d781e7
AdiShamir <6adish2@gmail.com>
2024-11-29 22:34:58 +0200

    Quartz sync: Nov 29, 2024, 10:34 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,11 +5,26 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'AdiShamir/Hive-Mind',
+        // from data-repo-id
+        repoId: 'R_kgDONWrDAg',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDONWrDAs4Cku_h',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Email: "mailto:6adish2@gmail.com",
+      Git: "https://github.com/AdiShamir",
+      LinkedIn: "https://www.linkedin.com/in/adi-shamir-07b854116/"
     },
   }),
 }
